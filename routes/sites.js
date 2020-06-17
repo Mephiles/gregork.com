6084b82d6f2e2bc34cbb55207df645d1f5acdc52
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-06-17 14:56:48 +0300

    Finish basic site

--- routes/sites.js
+++ routes/sites.js
@@ -16,41 +16,21 @@ const Log = require("../models/log");
 
 router.get("/", function(req, res){
     console.log("Loading INDEX");
-    res.render("index", {site: "home"});
+    res.render("index");
 });
 
 router.get("/education", function(req, res){
     console.log("Loading EDUCATION");
-    res.render("education", {site: "education"});
+    res.render("education");
 });
 
 router.get("/projects", function(req, res){
     console.log("Loading PROJECTS");
-    res.render("projects", {site: "projects"});
+    res.render("projects");
 });
 
-router.get("/logs", function(req, res){
-    console.log("Loading LOGS");
-
-    Log.find({}, function(err, data){
-        if(err){
-            console.log("ERROR", err);
-            req.flash("error", err.message);
-            return res.redirect("/");
-        }
-        
-        res.render("logs", {data: data});
-    });
+router.get("/projects/:name", function(req, res){
+    console.log("Loading PROJECTS: ", req.params.name);
+    res.render("projects/"+req.params.name)
 });
-
-// FUNCTIONS
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    console.log("User not logged in: redirecting.")
-    req.flash("error", "Please log in!")
-    res.redirect("/login");
-}
-
 module.exports = router;
